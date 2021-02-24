e5d0f0ea7770e8e598ac29b81c03afb7717b508f
nmarsollier <nmarsollier@gmail.com>
2021-02-24 12:32:09 -0300

    Ejemplos

--- go_funcional/dao/hello_dao.go
+++
@@ -1,8 +0,0 @@
-package dao
-
-// Hello esta funcion en prod va a funcionar
-// sin problemas, una funcion, una responsabilidad
-// programacion funcional, todo tiene sentido
-func Hello() string {
-	return "Holiiiiis"
-}
--- go_funcional/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/nmarsollier/go_di_ioc/go_funcional
-
-go 1.14
-
-require github.com/stretchr/testify v1.6.1
